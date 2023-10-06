0ebc76f8fa26736a289070ae39dd388932e520b2
Michelle Mounde <mitchmounde@gmail.com>
2023-10-06 16:07:05 +0300

    Create a bug model

--- bug/models.py
+++ bug/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
+class Bug(models.Model):
+    description = models.TextField()
+    bug_type = models.CharField(max_length=200)
+    report_date = models.DateField(default=date.today)
+    status = models.CharField(max_length=200)
