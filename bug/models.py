a8c721f15ffe5c2173048d41c385cbe5b8dea5e0
Michelle Mounde <mitchmounde@gmail.com>
2023-10-06 16:20:34 +0300

    Add __str__() method to bug model

--- bug/models.py
+++ bug/models.py
@@ -8,3 +8,6 @@ class Bug(models.Model):
     bug_type = models.CharField(max_length=200)
     report_date = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
