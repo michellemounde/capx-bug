f13cd8a6bd37a5ca26f704232c0a3b4fbe4acaca
Michelle Mounde <mitchmounde@gmail.com>
2023-10-17 11:26:12 +0300

    Remove model manager to keep model simple

--- bug/models.py
+++ bug/models.py
@@ -4,20 +4,12 @@ from django.db import models
 from django.utils import timezone
 
 
-class BugManager(models.Manager):
-    def create_bug(self, description, bug_type, status):
-        bug = self.create(description=description, bug_type=bug_type, status=status)
-        return bug
-
-
 # Create your models here.
 class Bug(models.Model):
     description = models.TextField()
     bug_type = models.CharField(max_length=50)
     report_date = models.DateTimeField(default=timezone.now)
     status = models.CharField(max_length=50)
-
-    objects = BugManager()
 
     def __str__(self):
         return self.description
