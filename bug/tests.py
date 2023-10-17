02a5f1cbaa1eeddd5d9b153d85b3a08f12548b2a
Michelle Mounde <mitchmounde@gmail.com>
2023-10-17 14:57:33 +0300

    Create test for was_reported_recently to show False for old bugs & to show True for recent bugs

--- bug/tests.py
+++ bug/tests.py
@@ -8,9 +8,27 @@ from .models import Bug
 
 # Create your tests here.
 class BugModelTests(TestCase):
+    def test_was_reported_recently_with_recent_bug(self):
+        """
+        was_reported_recently() returns True for bugs whose report_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        description = "Testing for was_reported_recently() on recent dates"
+        recent_bug = Bug(description=description, bug_type="error", status="todo", report_date=time)
+        self.assertIs(recent_bug.was_reported_recently(), True)
+
+    def test_was_reported_recently_with_old_bug(self):
+        """
+        was_reported_recently() returns False for bugs whose report_date is older than a day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        description = "Testing for was_reported_recently() on old dates"
+        old_bug = Bug(description=description, bug_type="error", status="todo", report_date=time)
+        self.assertIs(old_bug.was_reported_recently(), False)
+
     def test_was_reported_recently_with_future_bug(self):
         """
-        was_reported_recently() returns False for questions whose report_date is in the future.
+        was_reported_recently() returns False for bugs whose report_date is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         description = "Testing for was_reported_recently() on future dates"
