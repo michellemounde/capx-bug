27ae54d9fbd108c5dd5e20d32da8323586e5d412
Michelle Mounde <mitchmounde@gmail.com>
2023-10-17 18:24:53 +0300

    Render report_date field in form to allow for setting of past and future dates

--- bug/forms.py
+++ bug/forms.py
@@ -5,3 +5,5 @@ class BugForm(forms.Form):
     description = forms.CharField(min_length=25, max_length=200, widget=forms.Textarea)
     bug_type = forms.CharField(min_length=4, max_length=50)
     status = forms.CharField(min_length=4, max_length=50)
+    report_date = forms.SplitDateTimeField(widget=forms.SplitDateTimeWidget
+                                           (date_attrs={"type": "date"}, time_attrs={"type": "time"}))
