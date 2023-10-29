9b676aeb54a648f584e923730545635e4da205a0
Michelle Mounde <mitchmounde@gmail.com>
2023-10-29 18:01:11 +0300

    Modify bug_type and status fields to ChoiceField's & Convert Form to Modelform

--- bug/forms.py
+++ bug/forms.py
@@ -1,9 +1,17 @@
 from django import forms
+from bug.models import Bug
 
 
-class BugForm(forms.Form):
-    description = forms.CharField(min_length=25, max_length=200, widget=forms.Textarea)
-    bug_type = forms.CharField(min_length=4, max_length=50)
-    status = forms.CharField(min_length=4, max_length=50)
-    report_date = forms.SplitDateTimeField(widget=forms.SplitDateTimeWidget
-                                           (date_attrs={"type": "date"}, time_attrs={"type": "time"}))
+class BugForm(forms.ModelForm):
+    class Meta:
+        model = Bug
+        fields = ["description", "bug_type", "report_date", "status"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["description"] = forms.CharField(widget=forms.Textarea)
+        self.fields["bug_type"] = forms.ChoiceField(choices=Bug.BUG_TYPE_CHOICES)
+        self.fields["status"] = forms.ChoiceField(choices=Bug.STATUS_CHOICES)
+        self.fields["report_date"] = forms.SplitDateTimeField(
+                                        widget=forms.SplitDateTimeWidget
+                                        (date_attrs={"type": "date"}, time_attrs={"type": "time"}))
--- bug/models.py
+++ bug/models.py
@@ -7,10 +7,57 @@ from django.utils import timezone
 
 # Create your models here.
 class Bug(models.Model):
+    FEATURE = "FEAT"
+    ERROR = "ERR"
+    FUNCTIONAL = "FUNC"
+    LOGICAL = "LOGIC"
+    WORKFLOW = "FLOW"
+    USABILITY = "UIUX"
+    SECURITY = "SEC"
+    PERFORMANCE = "PERF"
+    COMPATIBILITY = "COMP"
+    UNIT = "UNIT"
+    SYSTEM = "SYS"
+    OUTOFBOUND = "OUT"
+    BUG_TYPE_CHOICES = [
+        (FEATURE, "New Feature"),
+        (ERROR, "Error"),
+        (FUNCTIONAL, "Functional"),
+        (LOGICAL, "Logical"),
+        (WORKFLOW, "Workflow"),
+        (USABILITY, "Usability"),
+        (SECURITY, "Security"),
+        (PERFORMANCE, "Performance"),
+        (COMPATIBILITY, "Compatibility"),
+        (UNIT, "Unit Level"),
+        (SYSTEM, "System Level"),
+        (OUTOFBOUND, "Out of Bound"),
+    ]
+
+    TODO = "TODO"
+    PLANNING = "PLAN"
+    DESIGNING = "DESIGN"
+    PROTOTYPING = "PROTO"
+    WIP = "WIP"
+    TESTING = "TEST"
+    RNA = "RNA"
+    STAGING = "STAGE"
+    MERGING = "MERGE"
+    STATUS_CHOICES = [
+        (TODO, "To-Do"),
+        (PLANNING, "Planning"),
+        (DESIGNING, "Designing"),
+        (PROTOTYPING, "Prototyping"),
+        (WIP, "Work in Progress"),
+        (TESTING, "Testing"),
+        (RNA, "Review & Approval"),
+        (STAGING, "Staging"),
+        (MERGING, "Merging"),
+    ]
     description = models.TextField(validators=[MinLengthValidator(25), MaxLengthValidator(200)])
-    bug_type = models.CharField(max_length=50, validators=[MinLengthValidator(4)])
+    bug_type = models.CharField(max_length=5, choices=BUG_TYPE_CHOICES)
     report_date = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=50, validators=[MinLengthValidator(4)])
+    status = models.CharField(max_length=6, choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.description
